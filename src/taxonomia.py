d67271a6c23bf2e32c8883bd6f6bbeb84dde932a
Daniel Precioso <danipg1409@gmail.com>
2021-02-02 22:22:16 +0100

    Read taxonomia

--- src/taxonomia.py
+++ src/taxonomia.py
@@ -1,25 +1,47 @@
+import numpy as np
 import pandas as pd
 
 PATH_TAXONOMIA = "datos_NPI/Taxonomía_23012021.xlsx"
 
 
-def return_all_medidas(
-    path_taxonomia=PATH_TAXONOMIA,
-        verbose=False
-):
-    """Returns a list of the relevant medidas"""
-
+def read_taxonomia(path_taxonomia=PATH_TAXONOMIA):
     xl = pd.ExcelFile(path_taxonomia)
 
     list_sheet = xl.sheet_names
-    list_codigos = []
+    list_df = []
+
+    col_rename = {
+        "Código medida concreta": "codigo",
+        "Medida concreta": "medida",
+        "Ponderación del item": "ponderacion",
+    }
 
     for sheet in list_sheet:
+        df = xl.parse(sheet).rename(col_rename, axis=1)
+        # Creamos las columnas "variable", que indica el ambito donde se aplican las
+        # medidas, e "item", que agrupa las medidas por items
+        df["variable"] = sheet
+        df["item"] = 1
         try:
-            df = xl.parse(sheet)
-            list_codigos += df["Código medida concreta"].unique().tolist()
+            # Si una fila no tiene informacion en "ponderacion" es porque pertenece al
+            # mismo item que la anterior fila
+            df.loc[df["ponderacion"].isna(), "item"] = 0
+            df["item"] = df["item"].cumsum()
+            df.fillna(method="ffill", inplace=True)
         except KeyError:
-            if verbose:
-                print(f"Sheet '{sheet}' omitted")
+            continue
+        list_df += [df]
 
-    return sorted(set(list_codigos))
+    df = pd.concat(list_df).reset_index(drop=True)
+
+    return df
+
+
+def return_all_medidas(path_taxonomia=PATH_TAXONOMIA):
+    """Returns a list of the relevant medidas"""
+
+    df = read_taxonomia(path_taxonomia=path_taxonomia)
+
+    list_codigos = df["codigo"].unique().tolist()
+
+    return list_codigos
