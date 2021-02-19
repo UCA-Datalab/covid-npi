75dfa003fb20a55140657cd66567d124129e9a83
Daniel Precioso <danipg1409@gmail.com>
2021-02-19 14:47:21 +0100

    Always index mongo by code of province

--- covidnpi/web/dataloaders.py
+++ covidnpi/web/dataloaders.py
@@ -3,13 +3,13 @@ from covidnpi.web.mongo import load_mongo
 
 
 def return_ambits_by_province(
-    province: str, ambits: tuple, path_config: str = "covidnpi/config.toml"
+    code: str, ambits: tuple, path_config: str = "covidnpi/config.toml"
 ):
     """Loads the scores stored in mongo for a given combination of province and ambits
 
     Parameters
     ----------
-    province : str
+    code : str
     ambits : tuple
     path_config : str, optional
 
@@ -25,7 +25,7 @@ def return_ambits_by_province(
     col = mongo.get_col("scores")
 
     x = col.find_one({"provincia": "fechas"})["x"]
-    dict_provincia = col.find_one({"provincia": province})
+    dict_provincia = col.find_one({"code": code})
 
     dict_plot = {}
 
@@ -35,27 +35,27 @@ def return_ambits_by_province(
         except KeyError:
             raise KeyError(f"Ambito '{ambit}' no existe")
         except TypeError:
-            raise KeyError(f"Provincia '{province}' no encontrada")
+            raise KeyError(f"Provincia '{code}' no encontrada")
         dict_plot.update({ambit: {"x": x, "y": y}})
 
     return dict_plot
 
 
 def return_provinces_by_ambit(
-    ambit: str, provinces: tuple, path_config: str = "covidnpi/config.toml"
+    ambit: str, codes: tuple, path_config: str = "covidnpi/config.toml"
 ):
     """Loads the scores stored in mongo for a given combination of provinces and ambit
 
     Parameters
     ----------
     ambit : str
-    provinces : tuple
+    codes : tuple
     path_config : str, optional
 
     Returns
     -------
     dict_plot : dict
-        {province: {x, y}}
+        {code: {x, y}}
         x are dates in string format, y are the score values
 
     """
@@ -67,25 +67,25 @@ def return_provinces_by_ambit(
 
     dict_plot = {}
 
-    for province in provinces:
-        dict_provincia = col.find_one({"provincia": province})
+    for code in codes:
+        dict_provincia = col.find_one({"code": code})
         try:
             y = dict_provincia[ambit]
         except KeyError:
             raise KeyError(f"Ambito '{ambit}' no existe")
         except TypeError:
-            raise KeyError(f"Provincia '{province}' no encontrada")
-        dict_plot.update({province: {"x": x, "y": y}})
+            raise KeyError(f"Provincia '{code}' no encontrada")
+        dict_plot.update({code: {"x": x, "y": y}})
 
     return dict_plot
 
 
-def return_casos_of_province(province: str, path_config: str = "covidnpi/config.toml"):
+def return_casos_of_province(code: str, path_config: str = "covidnpi/config.toml"):
     """Loads the number of cases stored in mongo for a given province
 
     Parameters
     ----------
-    province : str
+    code : str
     path_config : str, optional
 
     Returns
@@ -99,19 +99,19 @@ def return_casos_of_province(province: str, path_config: str = "covidnpi/config.
     mongo = load_mongo(cfg_mongo)
     col = mongo.get_col("casos")
 
-    x = col.find_one({"provincia": province})
+    x = col.find_one({"code": code})
     dict_plot = {"x": x["fechas"], "y": x["casos"]}
     return dict_plot
 
 
 def return_crecimiento_of_province(
-    province: str, path_config: str = "covidnpi/config.toml"
+    code: str, path_config: str = "covidnpi/config.toml"
 ):
     """Loads the growth of cases stored in mongo for a given province
 
     Parameters
     ----------
-    province : str
+    code : str
     path_config : str, optional
 
     Returns
@@ -125,6 +125,6 @@ def return_crecimiento_of_province(
     mongo = load_mongo(cfg_mongo)
     col = mongo.get_col("casos")
 
-    x = col.find_one({"provincia": province})
+    x = col.find_one({"code": code})
     dict_plot = {"x": x["fechas"], "y": x["crecimiento"]}
     return dict_plot
