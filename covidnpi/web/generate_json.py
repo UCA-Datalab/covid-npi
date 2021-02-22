34462a4dcb253d8f39986f1ac0ae6588e43b6395
Daniel Precioso <danipg1409@gmail.com>
2021-02-22 12:35:02 +0100

    Fix error in json_ambitos

--- covidnpi/web/generate_json.py
+++ covidnpi/web/generate_json.py
@@ -43,7 +43,7 @@ def json_ambitos(path_config: str, path_json: str):
     mongo = load_mongo(cfg_mongo)
     col = mongo.get_col("scores")
     dict_provincia = col.find_one({"code": "M"})
-    list_ambitos = [v for v in dict_provincia.values]
+    list_ambitos = [v for v in dict_provincia.values()]
     list_ambitos.remove("M")
     list_ambitos.remove("fechas")
 
