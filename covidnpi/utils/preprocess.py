effba0e5fe54310eb3a9292717699801110985bb
daniprec <danipg1409@gmail.com>
2021-04-07 10:15:06 +0200

    Avoid NaN in for word loop

--- covidnpi/utils/preprocess.py
+++ covidnpi/utils/preprocess.py
@@ -178,7 +178,7 @@ def rename_unidad(df, rename: dict = DICT_UNIDAD_RENAME):
     # If any value contains the exact word, change value to word
     list_rename = set(rename.values())
     for word in list_rename:
-        if len(word) == 0:
+        if type(word) is float:
             continue
         mask_word = df["unidad"].fillna("NA").str.contains(word, case=False)
         df.loc[mask_word, "unidad"] = word
