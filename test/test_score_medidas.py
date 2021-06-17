5eb8597d01854deef2e99cb35091b416e0038f95
daniprec <daniel.precioso@uca.es>
2021-06-17 10:54:29 +0200

    Test do not store additional files

--- test/test_score_medidas.py
+++ test/test_score_medidas.py
@@ -7,12 +7,7 @@ from covidnpi.utils.taxonomia import return_taxonomia
 def test_score_medidas(
     path_medidas: str = "test/data/medidas.csv",
     path_taxonomia: str = "test/data/taxonomia.xlsx",
-    path_tax_out: str = "test/data/taxonomia.csv",
-    path_score: str = "test/data/score_medidas.csv",
 ):
-    taxonomia = return_taxonomia(
-        path_taxonomia=path_taxonomia, path_output=path_tax_out
-    )
+    taxonomia = return_taxonomia(path_taxonomia=path_taxonomia, path_output=None)
     med = pd.read_csv(path_medidas)
     sc_med = score_medidas(med, taxonomia)
-    sc_med.to_csv(path_score)
