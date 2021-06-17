2aa86263d465c1315ce8326f60c1e4986c711191
daniprec <daniel.precioso@uca.es>
2021-06-17 18:15:00 +0200

    Fix path in update script

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 rm -r output/
-python covidnpi/preprocess_and_score.py --path-raw ../modelos-covid/datos_NPI_3 > log.out
+python covidnpi/preprocess_and_score.py --path-raw datos_NPI
 python covidnpi/initialize_web.py --path-config config.toml 
 python covidnpi/initialize_web.py --path-config config-staging.toml 
 python covidnpi/initialize_web.py --path-config config-live.toml 
