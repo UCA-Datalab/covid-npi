8ddc92dbde0ea9801d51efb287d97ee9c44f7b5a
daniprec <daniel.precioso@uca.es>
2021-06-19 11:44:03 +0200

    Function names

--- covidnpi/score/score_islas.py
+++ covidnpi/score/score_islas.py
@@ -1,10 +1,11 @@
 from functools import reduce
 
+import pandas as pd
 from covidnpi.utils.log import logger
 from covidnpi.utils.regions import ISLA_TO_PERCENTAGE
 
 
-def store_score_isle(dict_islas: dict, dict_ambito: dict):
+def aggregate_score_isles(dict_islas: dict, dict_ambito: dict) -> pd.DataFrame:
     list_df = []
     for isle, percentage in dict_islas.items():
         try:
@@ -14,14 +15,14 @@ def store_score_isle(dict_islas: dict, dict_ambito: dict):
     return reduce(lambda x, y: x.add(y, fill_value=0), list_df)
 
 
-def return_dict_score_islas(dict_ambito: dict):
+def return_dict_score_islas(dict_ambito: dict) -> dict:
     dict_ccaa = {}
     # Ensure the isles of each group are present
     # If so, perform grouping operation
     for ccaa, dict_islas in ISLA_TO_PERCENTAGE.items():
         logger.debug(ccaa)
         try:
-            df = store_score_isle(dict_islas, dict_ambito)
+            df = aggregate_score_isles(dict_islas, dict_ambito)
             dict_ccaa.update({ccaa: df})
         except KeyError as er:
             logger.error(f"No se pudo calcular {ccaa}. {er}")
