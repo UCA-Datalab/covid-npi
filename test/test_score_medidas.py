96a5914a7656ddb1c8fdeda361b297ab4cb37526
daniprec <daniel.precioso@uca.es>
2021-06-23 17:57:30 +0200

    Improve test score medidas

--- test/test_score_medidas.py
+++ test/test_score_medidas.py
@@ -1,13 +1,29 @@
 import pandas as pd
-
+import pytest
 from covidnpi.score.score_medidas import score_medidas
 from covidnpi.utils.taxonomia import return_taxonomia
 
 
+@pytest.fixture
+def medidas() -> pd.DataFrame:
+    path_medidas = "test/data/medidas.csv"
+    yield pd.read_csv(path_medidas)
+
+
+@pytest.fixture
+def taxonomia() -> pd.DataFrame:
+    path_taxonomia = "test/data/taxonomia.xlsx"
+    yield return_taxonomia(path_taxonomia=path_taxonomia, path_output=None)
+
+
+@pytest.fixture
+def sc_medidas() -> pd.DataFrame:
+    path_score = "test/data/score_medidas.csv"
+    yield pd.read_csv(path_score, parse_dates=["fecha"])
+
+
 def test_score_medidas(
-    path_medidas: str = "test/data/medidas.csv",
-    path_taxonomia: str = "test/data/taxonomia.xlsx",
+    medidas: pd.DataFrame, taxonomia: pd.DataFrame, sc_medidas: pd.DataFrame
 ):
-    taxonomia = return_taxonomia(path_taxonomia=path_taxonomia, path_output=None)
-    med = pd.read_csv(path_medidas)
-    sc_med = score_medidas(med, taxonomia, path_out_conditions=None)
+    sc_med = score_medidas(medidas, taxonomia, path_out_conditions=None).reset_index()
+    pd.testing.assert_frame_equal(sc_med, sc_medidas, check_names=False)
