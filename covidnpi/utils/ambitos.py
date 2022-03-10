0ec5f875a3957bdeae823c7d54537717d5682941
daniprec <daniel.precioso@uca.es>
2022-03-10 12:18:12 +0100

    Raise error when no ambitos.json is found

--- covidnpi/utils/ambitos.py
+++ covidnpi/utils/ambitos.py
@@ -4,12 +4,13 @@ from typing import Iterable
 
 
 def list_ambitos(path_data: Path) -> Iterable[str]:
-    """List the areas currently available
+    """List the areas currently available, using the file
+    'ambitos.json'
 
     Parameters
     ----------
     path_data : Path
-        Path to data folder
+        Path to data folder, containing 'ambitos.json' file
 
     Returns
     -------
@@ -17,7 +18,12 @@ def list_ambitos(path_data: Path) -> Iterable[str]:
         List of areas
     """
     path_json = path_data / "ambitos.json"
-    with open(path_json) as f:
-        dict_areas = json.load(f)
-        list_areas = [d["value"] for d in dict_areas]
+    try:
+        with open(path_json) as f:
+            dict_areas = json.load(f)
+            list_areas = [d["value"] for d in dict_areas]
+    except FileNotFoundError as err:
+        raise FileNotFoundError(
+            f"{err}\n[IMPORTANT] Run `covidnpi/web/generate_json.py` first!"
+        )
     return list_areas
