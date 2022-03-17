730e2314651af2b4eca06f36bdb638e63bb46c07
daniprec <daniel.precioso@uca.es>
2022-03-17 10:55:05 +0100

    Load boxplots in mongo and update names

--- covidnpi/web/dataloaders.py
+++ covidnpi/web/dataloaders.py
@@ -6,7 +6,7 @@ from covidnpi.web.mongo import load_mongo
 DATE_MIN = "2020-07-01"
 
 
-def return_fields_by_province(
+def return_scores_of_fields_by_province(
     code: str, fields: tuple, path_config: str = "covidnpi/config.toml"
 ) -> Dict:
     """Loads the scores stored in mongo for a given combination of province and fields
@@ -69,7 +69,7 @@ def return_fields_by_province(
     return dict_plot
 
 
-def return_provinces_by_field(
+def return_scores_of_provinces_by_field(
     field: str, codes: tuple, path_config: str = "covidnpi/config.toml"
 ) -> Dict:
     """Loads the scores stored in mongo for a given combination of provinces and field
@@ -206,7 +206,7 @@ def return_growth_of_province(
     return dict_plot
 
 
-def return_field_statistics_by_province(
+def return_statistics_of_field_by_province(
     code: str, path_config: str = "covidnpi/config.toml"
 ) -> List[Dict]:
     """Loads the list of statistics by field, for a given province
@@ -239,3 +239,45 @@ def return_field_statistics_by_province(
         r.append(r[0])
         list_plot.append({"r": r, "theta": list_fields, "name": key})
     return list_plot
+
+
+def return_scores_boxplot_of_field(
+    code: str, path_config: str = "covidnpi/config.toml"
+) -> List[Dict]:
+    """Loads the list of boxplot for a given field of activity
+
+    Parameters
+    ----------
+    code : str
+        Field of activity code
+    path_config : str, optional
+        Path to config, by default "covidnpi/config.toml"
+
+    Returns
+    -------
+    List[Dict]
+        List of dictionaries with format {"x": List[str], "y": List[float], "color": str, "name": str}
+    """
+    cfg_mongo = load_config(path_config, key="mongo")
+    mongo = load_mongo(cfg_mongo)
+    col = mongo.get_col("scores")
+
+    x = col.find_one({"id": "boxplot"})
+    list_dates = x["dates"]
+
+    try:
+        dict_field = x[code]
+    except KeyError:
+        print(f"[ERROR] Field '{code}' not found in boxplot.")
+        return [{"x": [], "y": [], "color": "#0000", "name": "No data"}]
+
+    list_out = []
+    # Loop through boxplot lines
+    for key, color in cfg_mongo["boxplot"].items():
+        try:
+            list_out.append(
+                {"x": list_dates, "y": dict_field[key], "color": color, "name": key}
+            )
+        except KeyError:
+            print(f"[ERROR] Key '{key}' not found in boxplot '{code}'.")
+    return list_out
